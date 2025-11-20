f80f7303a2fe6f74945d628994f1f134f61e4028
Salem <loucks@umich.edu>
2025-11-20 00:01:34 -0500

    working noise + sim curve

--- param_utils.py
+++ param_utils.py
@@ -16,7 +16,7 @@ class Param:
         if self.name == "u_0":
             x = np.sqrt(np.random.uniform(inmin, inmax))
         elif self.name == "alpha":
-            x = np.random.normal(inmin, inmax)
+            x = np.random.uniform(inmin, inmax)
         else:
             x = 10**np.random.uniform(inmin, inmax)
 
@@ -34,51 +34,63 @@ class Param:
             s = self.get_sample(inmin, inmax)
             samples.append(s[0])
             labels.append(s[1])
-        return samples, labels
+        return np.array(samples), np.array(labels)
     
     def get_sample(self, inmin, inmax):
         x = self.cutoff(inmin, inmax)
         lab = self.get_label(x)
-        return [lab, x]
+        return [x, lab]
 
     def get_label(self, x):
         return str(f"{self.name}: " + "{:.2f}".format(x))
+    
+    def get_label_n(self, x):
+        return [self.get_label(x)]
 
 class Params:
     def __init__(self):
         """u_0, t_E, rho, q, s, alpha"""
-        self.u_0 = Param("u_0", 0, np.sqrt(1), 0.5)
-        self.t_E = Param("t_E", 3, 100, 50)
-        self.rho = Param("rho", 10e-8, 10e-3, 10e-4)
-        self.q = Param("q", 10e-5, 1, 10e-3)
-        self.s = Param("s", -0.1, 10, 1)
-        self.alpha = Param("alpha", 0, 360, 180)
+        self.u_0 = Param("u_0", 0, 2, 0.5)
+        self.t_E = Param("t_E", 3, 50, 50)
+        self.rho = Param("rho", 0.009, 0.1, 0.01) # from Earth size to Jupiter size
+        self.density = Param("density", 0.33, 4.3, 1.5)
+        self.q = Param("q", 0.33 * 0.009, 4.3 * 0.1, 0.01 * 1.5)
+        self.s = Param("s", 0, 5, 1)
+        self.alpha = Param("alpha", 0, 180, 90)
 
     def get_vals(self, n):
+        rhos = self.rho.get_sample_n(-3, -1, n)
+        q_vals = self.density.get_sample_n(-0.5, 0.65, n)[0] * rhos[0]
+        q_labs = self.q.get_label_n(q_vals)
+
         all_data = [
             self.u_0.get_sample_n(0, 1, n), self.t_E.get_sample_n(0.5, 2, n),
-            self.rho.get_sample_n(-10, -3, n), self.q.get_sample_n(-5, 0, n), 
-            self.s.get_sample_n(-1, 1, n), self.alpha.get_sample_n(180, 40, n)
+            rhos, (q_vals, q_labs), 
+            self.s.get_sample_n(-1, 1, n), self.alpha.get_sample_n(0, 180, n)
         ]
         vals = [dat[1] for dat in all_data]
         labels = [dat[0] for dat in all_data]
         return vals, labels
     
     def get_vals(self):
+        rho = self.rho.get_sample(-3, -1)
+        q_val = self.density.get_sample(-0.5, 0.65)[0] * rho[0]
+        q_lab = self.q.get_label(q_val)
+        
         all_data = [
             self.u_0.get_sample(0, 1), self.t_E.get_sample(0.5, 2),
-            self.rho.get_sample(-10, -3), self.q.get_sample(-5, 0), 
-            self.s.get_sample(-1, 1), self.alpha.get_sample(180, 40)
+            rho, (q_val, q_lab), 
+            self.s.get_sample(-1, 1), self.alpha.get_sample(0, 180)
         ]
-        vals = [dat[1] for dat in all_data]
-        labels = [dat[0] for dat in all_data]
+        vals = [dat[0] for dat in all_data]
+        labels = [dat[1] for dat in all_data]
         return vals, labels
     
     def get_inits(self):
         return [self.u_0.init,
                 self.t_E.init,
                 self.rho.init,
-                self.q.init,
+                self.density.init * self.rho.init,
                 self.s.init,
                 self.alpha.init]
     
